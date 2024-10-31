31c6f3bf58bdd6dd21a3c6e5a4218702d3a0fd6b
kmw10693 <kmw106933@naver.com>
2024-10-31 16:30:42 +0900

    refactor: 예외 status 상태 코드 추가

--- backend/src/main/java/ku_rum/backend/global/response/status/BaseExceptionResponseStatus.java
+++ backend/src/main/java/ku_rum/backend/global/response/status/BaseExceptionResponseStatus.java
@@ -15,7 +15,18 @@ public enum BaseExceptionResponseStatus implements ResponseStatus{
      * 200: Request 오류 (BAD_REQUEST)
      */
     BAD_REQUEST(200, HttpStatus.BAD_REQUEST, "유효하지 않은 요청입니다."),
-    URL_NOT_FOUND(201, HttpStatus.BAD_REQUEST, "유효하지 않은 URL 입니다.");
+    URL_NOT_FOUND(201, HttpStatus.BAD_REQUEST, "유효하지 않은 URL 입니다."),
+
+    /**
+     * 300: User 오류
+     */
+    DUPLICATE_EMAIL(300, HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
+    DUPLICATE_STUDENT_ID(301, HttpStatus.BAD_REQUEST, "이미 존재하는 학번입니다."),
+
+    /**
+     * 400: Department 오류
+     */
+    NO_SUCH_DEPARTMENT(400, HttpStatus.BAD_REQUEST, "존재하지 않는 학과명입니다.");
 
     private final int code;
     private final HttpStatus status;
