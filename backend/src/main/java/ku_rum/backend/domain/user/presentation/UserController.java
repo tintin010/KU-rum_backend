9d575ca7ddc2fff6e2ca3148b6e93ee3a3ba1593
kmw10693 <kmw106933@naver.com>
2024-11-02 02:10:09 +0900

    refactor: 아이디 중복 검증, 메일 전송 및 인증번호 확인 코드 추가

--- backend/src/main/java/ku_rum/backend/domain/user/presentation/UserController.java
+++ backend/src/main/java/ku_rum/backend/domain/user/presentation/UserController.java
@@ -1,8 +1,13 @@
 package ku_rum.backend.domain.user.presentation;
 
 import jakarta.validation.Valid;
+import ku_rum.backend.domain.user.application.MailService;
 import ku_rum.backend.domain.user.application.UserService;
+import ku_rum.backend.domain.user.dto.request.MailSendRequest;
+import ku_rum.backend.domain.user.dto.request.EmailValidationRequest;
+import ku_rum.backend.domain.user.dto.request.MailVerificationRequest;
 import ku_rum.backend.domain.user.dto.request.UserSaveRequest;
+import ku_rum.backend.domain.user.dto.response.MailVerificationResponse;
 import ku_rum.backend.domain.user.dto.response.UserSaveResponse;
 import ku_rum.backend.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +19,6 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @Validated
 public class UserController {
-
     private final UserService userService;
 
     @PostMapping
@@ -22,4 +26,20 @@ public class UserController {
         return BaseResponse.ok(userService.saveUser(userSaveRequest));
     }
 
+    @PostMapping("/validations/email")
+    public BaseResponse<Void> validateEmail(@RequestBody @Valid final EmailValidationRequest emailValidationRequest) {
+        userService.validateEmail(emailValidationRequest);
+        return BaseResponse.ok(null);
+    }
+
+    @PostMapping("me/mails")
+    public BaseResponse<Void> sendMail(@RequestBody @Valid final MailSendRequest mailSendRequest) {
+        userService.sendCodeToEmail(mailSendRequest);
+        return BaseResponse.ok(null);
+    }
+
+    @GetMapping("me/mail_verifications")
+    public BaseResponse<MailVerificationResponse> verificationEmail(@RequestBody @Valid final MailVerificationRequest mailVerificationRequest) {
+        return BaseResponse.ok(userService.verifiedCode(mailVerificationRequest));
+    }
 }
