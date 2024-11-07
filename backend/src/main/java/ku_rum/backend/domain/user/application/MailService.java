06ffdf1d82ca473c5ef15e17b118c2664774f5b2
kmw10693 <kmw106933@naver.com>
2024-11-07 21:17:50 +0900

    refactor: 메일 서비스 분리

--- backend/src/main/java/ku_rum/backend/domain/user/application/MailService.java
+++ backend/src/main/java/ku_rum/backend/domain/user/application/MailService.java
@@ -1,21 +1,35 @@
 package ku_rum.backend.domain.user.application;
 
-import jakarta.transaction.Transactional;
+import ku_rum.backend.domain.user.domain.repository.UserRepository;
+import ku_rum.backend.domain.user.dto.request.MailSendRequest;
+import ku_rum.backend.domain.user.dto.request.MailVerificationRequest;
+import ku_rum.backend.domain.user.dto.response.MailVerificationResponse;
+import ku_rum.backend.global.exception.user.DuplicateEmailException;
 import ku_rum.backend.global.exception.user.MailSendException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.time.Duration;
+import java.util.Random;
+
+import static ku_rum.backend.domain.user.domain.MailSendSetting.MAIL_SEND_INFO;
 import static ku_rum.backend.global.response.status.BaseExceptionResponseStatus.*;
 
 @Slf4j
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MailService {
     private final JavaMailSender emailSender;
+    private final UserRepository userRepository;
+    private final RedisTemplate<String, String> redisTemplate;
 
     public void sendEmail(String toEmail,
                           String title,
@@ -30,6 +44,64 @@ public class MailService {
         }
     }
 
+    public void sendCodeToEmail(final MailSendRequest mailSendRequest) {
+        validateDuplicateEmail(mailSendRequest.getEmail());
+
+        String title = MAIL_SEND_INFO.getTitle();
+        String authCode = this.createCode();
+
+        sendEmail(mailSendRequest.getEmail(), title, authCode);
+        storeAuthCode(mailSendRequest.getEmail(), authCode);
+    }
+
+    public MailVerificationResponse verifiedCode(final MailVerificationRequest mailVerificationRequest) {
+        String email = mailVerificationRequest.getEmail();
+        String authCode = mailVerificationRequest.getCode();
+
+        validateDuplicateEmail(email);
+        return MailVerificationResponse.of(isValidAuthCode(generateKeyByEmail(email), authCode));
+    }
+
+    private void storeAuthCode(String email, String authCode) {
+        redisTemplate.opsForValue().set(generateKeyByEmail(email),
+                authCode, getAuthCodeExpirationMills());
+    }
+
+    private Duration getAuthCodeExpirationMills() {
+        return Duration.ofMillis(MAIL_SEND_INFO.getAuthCodeExpirationMillis());
+    }
+
+    private String generateKeyByEmail(String email) {
+        return MAIL_SEND_INFO.getAUTH_CODE_PREFIX() + email;
+    }
+
+    private String getRedisAuthCode(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    private boolean checkExistsValue(String key) {
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+    }
+
+    private boolean isValidAuthCode(String key, String authCode) {
+        return checkExistsValue(key) && getRedisAuthCode(key).equals(authCode);
+    }
+
+    private String createCode() {
+        try {
+            Random random = SecureRandom.getInstanceStrong();
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < MAIL_SEND_INFO.getCodeLength(); i++) {
+                builder.append(random.nextInt(10));
+            }
+            return builder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            log.debug("네자리 난수 생성 시 예외가 발생했습니다.");
+            throw new MailSendException(INVALID_AUTH_CODE_GENERATION);
+        }
+    }
+
     private SimpleMailMessage createEmailForm(String toEmail,
                                               String title,
                                               String text) {
@@ -40,4 +112,11 @@ public class MailService {
 
         return message;
     }
+
+    private void validateDuplicateEmail(final String email) {
+        if (userRepository.existsByEmail(email)) {
+            throw new DuplicateEmailException(DUPLICATE_EMAIL);
+        }
+    }
+
 }
