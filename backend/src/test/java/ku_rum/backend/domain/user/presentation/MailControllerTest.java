5492672556b77d87eadedc8506cf3c7430f569e8
kmw10693 <kmw106933@naver.com>
2024-11-07 21:17:12 +0900

    test: 메일 컨트롤러 테스트 코드 분리

---
+++ backend/src/test/java/ku_rum/backend/domain/user/presentation/MailControllerTest.java
@@ -0,0 +1,68 @@
+package ku_rum.backend.domain.user.presentation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ku_rum.backend.domain.user.application.MailService;
+import ku_rum.backend.domain.user.dto.request.MailSendRequest;
+import ku_rum.backend.domain.user.dto.request.MailVerificationRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MailController.class)
+class MailControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private MailService mailService;
+
+    @DisplayName("이메일 인증 요청을 보낸다.")
+    @Test
+    void sendMail() throws Exception {
+        //given
+        MailSendRequest mailSendRequest = new MailSendRequest("kmw10693@konkuk.ac.kr");
+
+        //when then
+        mockMvc.perform(post("/api/v1/mails")
+                        .content(objectMapper.writeValueAsString(mailSendRequest))
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("OK"));
+    }
+
+    @DisplayName("이메일 인증 코드를 검증한다.")
+    @Test
+    void verificationEmail() throws Exception {
+        //given
+        MailVerificationRequest mailVerificationRequest = new MailVerificationRequest("kmw10693@konkuk.ac.kr", "1234");
+
+        //when then
+        mockMvc.perform(get("/api/v1/mails/mail_verifications")
+                        .content(objectMapper.writeValueAsString(mailVerificationRequest))
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("OK"));
+    }
+}
