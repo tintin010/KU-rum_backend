3c426cbc3159f6a96e500fe9d27d773f55f6ef4f
janghyunjun <buzz03312@naver.com>
2024-11-08 19:34:42 +0900

    Refactor: Notice 엔티티의 primary key를 url로 변경

--- backend/src/main/java/ku_rum/backend/domain/notice/domain/repository/NoticeRepository.java
+++ backend/src/main/java/ku_rum/backend/domain/notice/domain/repository/NoticeRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
     List<Notice> findByNoticeCategory(NoticeCategory noticeCategory);
+
+    Optional<Notice> findByUrl(String link);
 }
