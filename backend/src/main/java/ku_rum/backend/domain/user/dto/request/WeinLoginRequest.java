53db40c93839ba36e375dd9ee5c5dc9b198c098c
tintin <tintin4601@gmail.com>
2024-11-10 19:53:19 +0900

    Refactor: 코드 수정

--- backend/src/main/java/ku_rum/backend/domain/user/dto/request/WeinLoginRequest.java
+++ backend/src/main/java/ku_rum/backend/domain/user/dto/request/WeinLoginRequest.java
@@ -1,10 +1,14 @@
 package ku_rum.backend.domain.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 public class WeinLoginRequest {
+    @NotBlank(message = "사용자 아이디는 필수입니다.")
     private String userId;
+
+    @NotBlank(message = "사용자 비밀번호는 필수입니다.")
     private String password;
 }
