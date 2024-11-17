768ffd402e4b1e427840d0582c882d4071374cce
kmw10693 <kmw106933@naver.com>
2024-11-17 22:24:44 +0900

    refactor: @Repository 추가

--- backend/src/main/java/ku_rum/backend/domain/department/domain/repository/DepartmentRepository.java
+++ backend/src/main/java/ku_rum/backend/domain/department/domain/repository/DepartmentRepository.java
@@ -2,9 +2,11 @@ package ku_rum.backend.domain.department.domain.repository;
 
 import ku_rum.backend.domain.department.domain.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
     boolean existsByName(String name);
     Optional<Department> findByName(String name);
