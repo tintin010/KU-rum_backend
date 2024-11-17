5f5565706ad9dc12e0189e70e5ec2779841f0a55
kmw10693 <kmw106933@naver.com>
2024-11-17 22:26:54 +0900

    refactor: 불필요한 import문 제거

--- backend/src/test/java/ku_rum/backend/domain/user/presentation/UserControllerTest.java
+++ backend/src/test/java/ku_rum/backend/domain/user/presentation/UserControllerTest.java
@@ -18,15 +18,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.mockito.ArgumentMatchers.any;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
