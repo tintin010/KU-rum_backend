8e6bfefe998a8713f73bfdda81614ce7677b5470
kmw10693 <kmw106933@naver.com>
2025-01-01 14:42:00 +0900

    refactor: 서비스 오타 수정

--- backend/src/main/java/ku_rum/backend/domain/friend/dto/response/FriendFindResponse.java
+++ backend/src/main/java/ku_rum/backend/domain/friend/dto/response/FriendFindResponse.java
@@ -1,24 +1,23 @@
 package ku_rum.backend.domain.friend.dto.response;
 
-import ku_rum.backend.domain.friend.domain.Friend;
 import ku_rum.backend.domain.user.domain.User;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class FriendFIndResponse {
+public class FriendFindResponse {
 
     private Long id;
     private String nickname;
 
     @Builder
-    private FriendFIndResponse(Long id, String nickname) {
+    private FriendFindResponse(Long id, String nickname) {
         this.id = id;
         this.nickname = nickname;
     }
 
-    public static FriendFIndResponse from(User user) {
-        return FriendFIndResponse.builder()
+    public static FriendFindResponse from(User user) {
+        return FriendFindResponse.builder()
                 .id(user.getId())
                 .nickname(user.getNickname())
                 .build();
