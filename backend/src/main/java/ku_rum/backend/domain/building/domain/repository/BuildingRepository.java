fd698b0eb41e96d0ac434b726b20c99e4e24a9ed
Minu Kim <kmw106933@naver.com>
2025-01-01 14:57:57 +0900

    refactor: 레포지토리 어노테이션 추가

--- backend/src/main/java/ku_rum/backend/domain/building/domain/repository/BuildingRepository.java
+++ backend/src/main/java/ku_rum/backend/domain/building/domain/repository/BuildingRepository.java
@@ -2,6 +2,8 @@ package ku_rum.backend.domain.building.domain.repository;
 
 import ku_rum.backend.domain.building.domain.Building;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BuildingRepository extends JpaRepository<Building, Long> {
 }
